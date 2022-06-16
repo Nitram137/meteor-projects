8be92b650c3fe8c71871611e20657f4dd78d94ad
Martin Németh <martin@revolin.io>
2022-06-16 12:05:16 +0200

    Implement publications

--- meteor-wallet/imports/ui/ContactList.jsx
+++ meteor-wallet/imports/ui/ContactList.jsx
@@ -1,42 +1,57 @@
-import React from "react";
+import React, { memo } from "react";
 import { ContactsCollection } from "../api/ContactsCollection";
-import { useTracker } from "meteor/react-meteor-data";
+import { useSubscribe, useFind } from "meteor/react-meteor-data";
 
 export const ContactList = () => {
-    const contacts = useTracker(() => {
-        return ContactsCollection.find({}, {sort: {createdAt: -1}}).fetch();
-    });
-    
-    const removeContact = (event, _id) => {
-      event.preventDefault();
-      Meteor.call('contacts.remove', {contactId: _id });
-    }
+  const isLoading = useSubscribe('allContacts');
+  const contacts = useFind(() => ContactsCollection.find({}, {sort: {createdAt: -1}}));
+  
+  const removeContact = (event, _id) => {
+    event.preventDefault();
+    Meteor.call('contacts.remove', {contactId: _id });
+  }
 
+  if(isLoading()) {
     return (
-      <div>
-        <div className="mt-10">
-          <h3 className="text-xs font-semibold tracking-wide text-gray-500 uppercase">
-            Contact List
-          </h3>
-          <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-            {contacts.map((person, personIdx) => (
-              <li key={personIdx} className="flex items-center justify-between py-4 space-x-3">
-                <div className="flex items-center flex-1 min-w-0 space-x-3">
-                  <div className="flex-shrink-0">
-                    <img className="w-10 h-10 rounded-full" src={person.imageUrl} alt="" />
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                    <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
-                  </div>
-                  <div>
-                    <a href="#" onClick={(e) => removeContact(e, person._id)} className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-grey-700 bg-white hover:bg-grey-50">Remove</a>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+    <div>
+      <div className="mt-10">
+        <h3 className="text-xs font-semibold tracking-wide text-gray-500 uppercase">Loading...</h3>
+      </div>
+    </div>
+    )
+  }
+
+  const ContactItem = memo(({ contact }) => {
+    return (
+      <li className="flex items-center justify-between py-4 space-x-3">
+      <div className="flex items-center flex-1 min-w-0 space-x-3">
+        <div className="flex-shrink-0">
+          <img className="w-10 h-10 rounded-full" src={contact.imageUrl} alt="" />
+        </div>
+        <div className="flex-1 min-w-0">
+          <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+          <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+        </div>
+        <div>
+          <a href="#" onClick={(e) => removeContact(e, contact._id)} className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-grey-700 bg-white hover:bg-grey-50">Remove</a>
         </div>
       </div>
+    </li>
     )
+  });
+
+  return (
+    <div>
+      <div className="mt-10">
+        <h3 className="text-xs font-semibold tracking-wide text-gray-500 uppercase">
+          Contact List
+        </h3>
+        <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+          {contacts.map((contact) => (
+            <ContactItem key={contact._id} contact={contact} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
